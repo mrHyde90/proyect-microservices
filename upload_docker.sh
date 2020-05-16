dfe2cbb3ecb4d13bcb97785caa446bc0f28766b6
EC2 Default User <ec2-user@ip-172-31-63-103.us-west-2.compute.internal>
2020-05-16 12:17:22 +0000

    almost finish

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=wolusiter1990/udacity_project4 
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+export DOCKER_ID_USER="wolusiter1990"
+docker login
+docker tag udacity_project4 $DOCKER_ID_USER/udacity_project4 
 
 # Step 3:
 # Push image to a docker repository
+docker push $DOCKER_ID_USER/udacity_project4 
